f64500c6086782a449ae90ba242a815cf376c096
JorguKjoseski <jorgu.koseski@gmail.com>
2021-07-27 20:57:43 +0200

    Valtech FrontEnd task upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# Valech_FrontEndTask
+
+## Table of Content
+1. [ General Info ]
+2. [ Technologies ]
+3. [ installation ]
+
+### General Info
+***
+Valtech junior Front End task. Тhis project is built with HTML CSS and JavaScript, without the use of  any libraries and frameworks.
+
+## Technologies
+* [ HTML ]
+* [ CSS ]
+* [ JAVA SCRIPT ]
+
+## Instalattion
+***
+
+$ git clone https:// URL path
+$ cd ../path/to/the/file
+$ start index.html
---
+++ assets/js/script.js
@@ -0,0 +1,92 @@
+const navSlide = () => {
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		//toggle nav
+		nav.classList.toggle('nav-active');
+		//animate links
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = ``;
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+			}
+		});
+		//burger animation
+		burger.classList.toggle('toggleBurger');
+	});
+};
+navSlide();
+
+//____________________________________________
+
+// on click send mail.
+
+const sendMail = () => {
+	window.location = 'mailto:mk.contac@valtech.com';
+	console.log('klik');
+};
+
+let btn = document.querySelector('.mail_btn');
+
+btn.addEventListener('click', () => {
+	sendMail();
+});
+
+//____________________________________________________
+
+// cards generator with constructor
+
+class Card {
+	constructor(_img, _title, _body) {
+		(this.img = _img), (this.title = _title), (this.body = _body);
+	}
+}
+
+let card1 = new Card(
+	'./assets/img/slika_1.png',
+	'Teaser title',
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda quam recusandae temporibus sapiente dolores esse ipsum magnam aliquid aspernatur!'
+);
+let card2 = new Card(
+	'./assets/img/slika_2.png',
+	'Teaser title',
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda'
+);
+let card3 = new Card(
+	'./assets/img/slika_3.png',
+	'Teaser title',
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda quam recusandae temporibus sapiente dolores '
+);
+let card4 = new Card(
+	'./assets/img/slika_4.png',
+	'Teaser title',
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus assumenda quam recusandae temporibus sapiente dolores esse ipsum magnam aliquid aspernatur!'
+);
+
+let cardArr = [ card1, card2, card3, card4 ];
+
+// insert card template
+
+cardArr.forEach((element, index) => {
+	console.log(element);
+	document.querySelector('.cardsLoader').innerHTML += `	
+	<article class="card">
+    <img src=${element.img} alt="card_img" style="width:100%">
+    <div class="container">
+         <h3><b>${element.title}</b></h3> 
+         <p>${element.body}</p> 
+		 <div class="card_footer">
+         <button class="card_btn">Read more</button>
+		 </div>
+    </div>
+</article>
+	`;
+});
+//______________________________________________________
+
+document.querySelector('.form_btn').addEventListener('click', (e) => {
+	e.preventDefault();
+});

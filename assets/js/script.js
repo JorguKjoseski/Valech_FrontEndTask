af0d14b281d1c0c43818fd31496bee6ef2ba0f77
JorguKjoseski <jorgu.koseski@gmail.com>
2021-08-11 20:00:36 +0200

    commit 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
+	const burger =this.document.querySelector('.burger');	
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
 
